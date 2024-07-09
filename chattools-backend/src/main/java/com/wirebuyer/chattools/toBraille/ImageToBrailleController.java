9ff20e132e3af540e7413a7e7464f3e3f170b7ef
wirebuyer <39298316+WireBuyer@users.noreply.github.com>
2024-07-09 04:11:41 -0400

    added basic Caddy support. adjusted backend api endpoints and frontend api call

--- chattools-backend/src/main/java/com/wirebuyer/chattools/toBraille/ImageToBrailleController.java
+++ chattools-backend/src/main/java/com/wirebuyer/chattools/toBraille/ImageToBrailleController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequestMapping("/api/")
 public class ImageToBrailleController {
 
     private final ImageToBrailleService imageToBrailleService;
