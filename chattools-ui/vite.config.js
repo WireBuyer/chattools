e8422d83cd6be1aba9b593367ca17e5fd9f1c230
wirebuyer <39298316+WireBuyer@users.noreply.github.com>
2024-09-30 01:46:49 -0400

    changes to allow a "dev profie" to caddy to make similar environments. docker runs with a prod profile

--- chattools-ui/vite.config.js
+++ chattools-ui/vite.config.js
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  logLevel: 'info',
   server: {
     // port: 517,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost',
         changeOrigin: true,
       }
     }
